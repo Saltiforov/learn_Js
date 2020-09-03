eed57cb4c96a629d5e2a90b172510a732ac8c48b
Misha <dufeechka@gmail.com>
2020-09-03 19:39:02 +0300

    add script Js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+//
+// let number = 5
+// const leftBorderWidth = 1
+//
+// number = 10
+// console.log(number) // 10
+//
+// const  obj ={ // свойства объекста
+//     name:"Jonn",
+//     age:25,
+//     isMarried:false
+// }
+
+// const  result = confirm("Ti tut?")
+
+// const answer = prompt("Вам есть 18?", "18")
+const  answer = []
+answer[0] = prompt('Как ваше имя? ', '');
+answer[0] = prompt('Как ваше фамилия? ', ''); // функция запроса ввода данных по типу alert
+
+document.write(answer);
