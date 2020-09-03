0ea4e67c50e7a9aadc2ebddeff78b8caa5f43629
Misha <dufeechka@gmail.com>
2020-09-03 20:10:39 +0300

    Первое практическое занятие(решено)

--- js/script.js
+++ js/script.js
@@ -14,9 +14,31 @@
 
 // const  result = confirm("Ti tut?")
 
-// const answer = prompt("Вам есть 18?", "18")
-const  answer = []
-answer[0] = prompt('Как ваше имя? ', '');
-answer[0] = prompt('Как ваше фамилия? ', ''); // функция запроса ввода данных по типу alert
+// // const answer = prompt("Вам есть 18?", "18")
+// const  answer = []
+// answer[0] = prompt('Как ваше имя? ', '');
+// answer[0] = prompt('Как ваше фамилия? ', ''); // функция запроса ввода данных по типу alert
+//
+// document.write(answer);
 
-document.write(answer);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+console.log(numberOfFilms)
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+}
+const answer = prompt('Один из последних просмотренных фильмов?', '')
+const answerMark = prompt('На сколько оцените его?', '')
+const answer2 = prompt('Один из последних просмотренных фильмов?', '')
+const answerMark2 = prompt('На сколько оцените его?', '')
+// const movies = {
+//     log:answer,
+//     mark:answerMark
+// }
+personalMovieDB.movies[answer] = answerMark
+personalMovieDB.movies[answer2] = answerMark2
+console.log(personalMovieDB)
+
