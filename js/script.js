dd49ba395422ff4edadd6c458f86797c871bd0ae
Misha <dufeechka@gmail.com>
2020-09-24 18:05:19 +0300

    Макет для практики ч.3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,61 @@
-"use strict"
+'use strict';
 
-let str = "some";
-let strObj = new String(str);
-
-console.log(typeof(str))
-console.log(typeof(strObj))
-
-console.dir([1,2,3]);
+let numberOfFilms
 
 
-const soldier = {
-  health:400,
-  armor:100,
-    sayHello:function (){
-      console.log("Hello")
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
-};
-
-const john = {
-    health: 100
 }
 
-const john = Object.create(soldier)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true
+};
 
-// john.__proto__ = soldier
-Object.setPrototypeOf(john,soldier); // создания прототипа объекта
-console.log(john)
+
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        if (a !== null || b !== null || a !== '' || b !== '' || a.length < 50){
+            personalMovieDB[a] = b
+        }else{
+            console.log('error')
+            i--;
+        }
+    }
+}
 
 
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов")
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("Вы классический зритель")
+    }else if(personalMovieDB.count >= 30){
+        console.log("Вы киноман")
+    }else {
+        console.log('error')
+    }
+}
+
+
+function showMyBD(hidden){
+    if (!hidden){
+        console.log(personalMovieDB)
+    }
+}
+showMyBD(personalMovieDB.privat)
+
+function writeYourGenres(){
+    for (let i = 1 ; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`)
+    }
+}
+writeYourGenres()
