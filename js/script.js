25e21d27f9014bb899b857b1ad03a0e288374b56
Misha <dufeechka@gmail.com>
2020-09-29 20:36:51 +0300

    Залил практикум и задачки с CodeWars

--- js/script.js
+++ js/script.js
@@ -1,89 +1,93 @@
-'use strict';
+"use strict"
 
-let numberOfFilms
+//1)
+//  let x = 5
+// alert(x++) // 6
+//
+// //2
+//
+// console.log([] + false + null + true)
+//
+// //3
+// // И ЗАПИНАЕТСЯ НА ЛЖИ
+// // ИЛИ ЗАПИНАЕТСЯ НА ПРАВЛЕ
+// let y = 1
+// let x = y = 2
+// console.log(x) // error
+//
+// //4
+// console.log([] + 1 + 2)  // -------
+
+//5
+
+// alert('1'[0]) // 10
+
+//6
+//
+// console.log(2 && 1 && null && 0 && undefined) // --
+
+//7 Да есть первое это булевое значение
+
+//8
+
+// CODEWARS TASK
 
 
 
+// function sort(n){
+//   let arr = []
+//  for (let i = 0; i < n; i++){
+//   arr.push(i)
+//  }
+//  return arr
+// }
+// console.log(sort(5))   // not complited
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start:
-        function() {
-            numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-            while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-                numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-            }
-        },
-    rememberMyFilms:
-        function() {
-            for (let i = 0; i < 2; i++) {
-                const a = prompt('Один из последних просмотренных фильмов?', ''),
-                    b = prompt('На сколько оцените его?', '');
-                if (a !== null || b !== null || a !== '' || b !== '' || a.length < 50) {
-                    personalMovieDB[a] = b
-                } else {
-                    console.log('error')
-                    i--;
-                }
-            }
-        },
-    detectPersonalLevel:
-        function() {
-            if (personalMovieDB.count < 10) {
-                console.log("Просмотрено довольно мало фильмов")
-            } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-                console.log("Вы классический зритель")
-            } else if (personalMovieDB.count >= 30) {
-                console.log("Вы киноман")
-            } else {
-                console.log('error')
-            }
-        },
-    showMyBD:
-        function(hidden) {
-            if (!hidden) {
-                console.log(personalMovieDB)
-            }
-        },
+// const stringName = prompt('Input your name','')
+//   const Obj = {
+//    hello: function (name){
+//     if (name){
+//      return `"Hello ${name}!"`
+//     }else {
+//      return "Hello world!"
+//     }
+//    }
+//   }
+//   Obj.hello('John')
 
-    writeYourGenres:
-        function() {
-            for (let i = 1; i <= 3; i++){
-                let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-                if (genre !== '' &&  genre !== null){
-                        personalMovieDB.genres[i - 1] = genre
-                }else {
-                    console.log('Введите что то');
-                    i--;
-                }
-            }
-            personalMovieDB.genres.forEach(function (item,i){
-                console.log(`Любимый жанр ${i + 1}  - это ${item}`)
+// function hello(name){
+//  if (name){
+//   return `Hello, ${name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase()}!`
+//  }else {
+//   return "Hello, world!"
+//  }
+// }
+// hello('john')
 
-            })
-        },
-    toggleVisibleMyBD:
-        function() {
-            (personalMovieDB.privat) ? personalMovieDB.privat = false : personalMovieDB.privat = true
-        }
+// function isDivideBy(a,b,c){
+//  if (a % b || a % c){
+//   return false
+//  }else {
+//   return true
+//  }
+// }
+// isDivideBy()
+//
+// (a % b || a % c) ? return false :  return true
+
+
+// function arr(n){
+//  let newArr = [];
+//  for (let i = 0; i < n; i++){
+//   newArr.push(i);
+//  }
+//  return newArr;
+// }
+
+function speed(n){
+   if (n !== 0){
+    return 80 / n
+   } else {
+
+   }
 }
-
-personalMovieDB.writeYourGenres();
-personalMovieDB.toggleVisibleMyBD();
-console.log(personalMovieDB.showMyBD(personalMovieDB.privat))
-console.log(personalMovieDB.genres)
-
-
-
-
-
-
-
-
-
-
-
