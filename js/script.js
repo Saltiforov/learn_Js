1ae68376b8a973c9e397a6722a8deb9eecc191f3
Misha <dufeechka@gmail.com>
2020-09-29 21:06:02 +0300

    Получение єлемментов со страници

--- js/script.js
+++ js/script.js
@@ -1,93 +1,21 @@
 "use strict"
 
-//1)
-//  let x = 5
-// alert(x++) // 6
-//
-// //2
-//
-// console.log([] + false + null + true)
-//
-// //3
-// // И ЗАПИНАЕТСЯ НА ЛЖИ
-// // ИЛИ ЗАПИНАЕТСЯ НА ПРАВЛЕ
-// let y = 1
-// let x = y = 2
-// console.log(x) // error
-//
-// //4
-// console.log([] + 1 + 2)  // -------
+const box = document.getElementById('box');
+console.log(box)
 
-//5
+const buttons = document.getElementsByTagName('button');
+console.log(buttons[1])
 
-// alert('1'[0]) // 10
+const circles = document.getElementsByClassName('circle')
 
-//6
-//
-// console.log(2 && 1 && null && 0 && undefined) // --
+console.log(circles)
 
-//7 Да есть первое это булевое значение
+const hearts = document.querySelectorAll('.heart');
 
-//8
+hearts.forEach(item => {
+    console.log(item)
+})
 
-// CODEWARS TASK
+const oneHeart = document.querySelector('.heart')
+console.log(oneHeart)
 
-
-
-// function sort(n){
-//   let arr = []
-//  for (let i = 0; i < n; i++){
-//   arr.push(i)
-//  }
-//  return arr
-// }
-// console.log(sort(5))   // not complited
-
-// const stringName = prompt('Input your name','')
-//   const Obj = {
-//    hello: function (name){
-//     if (name){
-//      return `"Hello ${name}!"`
-//     }else {
-//      return "Hello world!"
-//     }
-//    }
-//   }
-//   Obj.hello('John')
-
-// function hello(name){
-//  if (name){
-//   return `Hello, ${name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase()}!`
-//  }else {
-//   return "Hello, world!"
-//  }
-// }
-// hello('john')
-
-// function isDivideBy(a,b,c){
-//  if (a % b || a % c){
-//   return false
-//  }else {
-//   return true
-//  }
-// }
-// isDivideBy()
-//
-// (a % b || a % c) ? return false :  return true
-
-
-// function arr(n){
-//  let newArr = [];
-//  for (let i = 0; i < n; i++){
-//   newArr.push(i);
-//  }
-//  return newArr;
-// }
-
-function speed(n){
-   if (n !== 0){
-    return 80 / n
-   } else {
-
-   }
-}
