fb7eddd62e61aa39f9cdcec7794a55ff2eb085d4
Saltiforov <dufeechka@gmail.com>
2020-10-16 11:25:46 +0300

    ДЕЛЕГИРОВАНИЕ!!!

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-"use strict"
+const btns = document.querySelectorAll('button');
+      wrapper = document.querySelector('.btn-block')
+// console.log(btns[0].classList.length)
+// console.log(btns[0].classList.item(0))
 
-const p = document.querySelectorAll('p')
-console.log(p)
+// console.log(btns[1].classList.add('red, fdsdffds'))
+// console.log(btns[0].classList.remove('blue'))
+// console.log(btns[0].classList.toggle('blue'))
 
-// defer сохраняет порядок загрузки джс файла, в хтмл
+// if (btns[1].classList.contains('red')){
+//     console.log('red');
+// }
+// contains проверка наличия элемента
 
 
+// btns[0].addEventListener('click', () =>{
+//     // if(!btns[1].classList.contains('red')) {
+//     //     btns[1].classList.add('red')
+//     // }else {
+//     //     btns[1].classList.remove('red')
+//     // }
+//     btns[1].classList.toggle('red')
+// })
+// toggle проверка на налачие события и (удаление/добавления его)
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false; // добавление постепенности загрузки
-    document.body.append(script);
-}
-loadScript("js/test.js")
-loadScript("js/some.js")
+// console.log(btns[0].className)
+
+//
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target)
+    if (event.target && event.target.matches("button.red")){
+         console.log('Hello')
+    }
+} )
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click' ,() => {
+//         console.log('Hello')
+//     })
+// })
+const btn = document.createElement('button')
+btn.classList.add('red')
+wrapper.append(btn)
