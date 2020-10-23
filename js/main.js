4d678384f584f3ba0ed294013016fe52d14d1d34
Saltiforov <dufeechka@gmail.com>
2020-10-23 16:25:45 +0300

    Timer on Js in page (2 version on me and lessons)

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const deadline = '2020-10-25'
+
+function getTime(deadline) {
+    const t = Date.parse(deadline) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(t / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(t / (1000 * 60) % 60),
+          seconds = Math.floor(t / 1000 % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+
+    }
+}
+function getZero(num){
+    if (num >= 0 && num < 10 ){
+        return `0${num}`
+    }else {
+        return num
+    }
+}
+function setClock(selector,deadline){
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timerInterval = setInterval(updateCLock, 1000)
+    updateCLock()
+    function updateCLock () {
+        const t = getTime(deadline)
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+    }
+    if (t.total <= 0){
+        clearInterval(timerInterval)
+    }
+}
+setClock('.timer',deadline)
+
+
+
+
